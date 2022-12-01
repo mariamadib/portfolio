c5fa63300db9556d0b156b5134b29b3b04f66709
Mariam Al Adib <mariam.al.adib1@gmail.com>
2022-12-01 07:53:53 +0200

    new files will adding

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ const menu = document.getElementById("menu");
 const action = document.getElementById("actions");
 
 menu.addEventListener("click", () => {
-    hundleMenu();
+    handleMenu();
 })
 
-function hundleMenu(){
+function handleMenu(){
     menu.classList.toggle("is-active");
     action.classList.toggle("is-active");
     
